d41c306204b92252586f8bdfe6db15ed90dfe3b6
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-06-11 10:09:48 +0530

    Update callback.py

--- DVIS/plugins/heroku/callback.py
+++ DVIS/plugins/heroku/callback.py
@@ -16,9 +16,9 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 HEROKU_API_URL = "https://api.heroku.com"
 HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")  # Pre-defined variable
-REPO_URL = "https://github.com/IamDvis/DV-MUSIC"  # Pre-defined variable
+REPO_URL = "http://github.com/Pbx-Official"  # Pre-defined variable
 BUILDPACK_URL = "https://github.com/heroku/heroku-buildpack-python"
-UPSTREAM_REPO = "https://github.com/IamDvis/DV-MUSIC"  # Pre-defined variable
+UPSTREAM_REPO = "http://github.com/Pbx-Official"  # Pre-defined variable
 UPSTREAM_BRANCH = "master"  # Pre-defined variable
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
